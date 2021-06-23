c7d432985de6e4f2d6024ce6a46e5559824b6b7e
LucaCain <LucaCain@users.noreply.github.com>
2021-06-23 20:32:49 +0000

    automated commit by check50 [check50=True]

---
+++ population.c
@@ -0,0 +1,43 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int start_size(void);
+
+int main(void)
+{
+ // TODO: Prompt for start size
+    int s = start_size();
+    
+    // TODO: Prompt for end size
+    int e;
+    do
+    {
+        e = get_int("End Size: ");
+    }
+    while (e < s || e == s);
+    
+    // TODO: Calculate number of years until we reach threshold
+    int n = 0;
+    do
+    {
+        s = (s/3) - (s/4) + s;
+        n++;
+
+    }
+    while (s < e);
+
+    // TODO: Print number of years
+    printf("Years: %i\n", n);
+}
+
+int start_size(void)
+{
+        int s;
+        do
+        {
+            s = get_int("Start Size: ");
+        }
+        while (s < 9);
+        return s;
+
+}
