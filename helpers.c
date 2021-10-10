a9c7109912b75f2ea0c58f741d4e6f89a481df98
LucaPiccone <LucaPiccone@users.noreply.github.com>
2021-10-10 13:16:20 +0000

    automated commit by check50 [check50=True]

--- helpers.c
+++ helpers.c
@@ -9,8 +9,8 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
         for (int j = 0; j < width; j++)
         {
             
-            image[i][j].rgbtBlue = image[i][j].rgbtRed;
-            image[i][j].rgbtGreen = image[i][j].rgbtBlue;
+            image[i][j].rgbtBlue = image[i][j].rgbtBlue;
+            image[i][j].rgbtGreen = image[i][j].rgbtGreen;
             image[i][j].rgbtRed = image[i][j].rgbtRed;
         }
     }
