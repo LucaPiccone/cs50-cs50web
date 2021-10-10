7bed3fae224c2bd8eda77b58d23877657a5f29f3
LucaPiccone <LucaPiccone@users.noreply.github.com>
2021-10-10 13:14:01 +0000

    automated commit by check50 [check50=True]

---
+++ helpers.c
@@ -0,0 +1,37 @@
+#include "helpers.h"
+#include <stdio.h>
+
+// Convert image to grayscale
+void grayscale(int height, int width, RGBTRIPLE image[height][width])
+{
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            
+            image[i][j].rgbtBlue = image[i][j].rgbtRed;
+            image[i][j].rgbtGreen = image[i][j].rgbtBlue;
+            image[i][j].rgbtRed = image[i][j].rgbtRed;
+        }
+    }
+
+    return;
+}
+
+// Reflect image horizontally
+void reflect(int height, int width, RGBTRIPLE image[height][width])
+{
+    return;
+}
+
+// Blur image
+void blur(int height, int width, RGBTRIPLE image[height][width])
+{
+    return;
+}
+
+// Detect edges
+void edges(int height, int width, RGBTRIPLE image[height][width])
+{
+    return;
+}
