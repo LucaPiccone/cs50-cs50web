6b13b68693d64e741d632c2cca4e8d8fb66099f1
LucaPiccone <LucaPiccone@users.noreply.github.com>
2021-10-10 13:54:25 +0000

    automated commit by check50 [check50=True]

--- helpers.c
+++ helpers.c
@@ -12,9 +12,9 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
             BYTE Blue = round((int)image[i][j].rgbtBlue);
             BYTE Green = round((int)image[i][j].rgbtGreen);
             BYTE Red = round((int)image[i][j].rgbtRed);
-            
-            BYTE average = round((int)Blue + Green + Red) / 3;
-            
+
+            BYTE average = round((int)Blue + Green + Red) / (int)3;
+
             image[i][j].rgbtBlue = average;
             image[i][j].rgbtGreen = average;
             image[i][j].rgbtRed = average;
