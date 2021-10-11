533d68b8e55cd9ac8203af798780cbd165d1b886
LucaPiccone <LucaPiccone@users.noreply.github.com>
2021-10-11 18:21:04 +0000

    automated commit by check50 [check50=True]

--- helpers.c
+++ helpers.c
@@ -9,12 +9,12 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
     {
         for (int j = 0; j < width; j++)
         {
-            BYTE Blue = image[i][j].rgbtBlue;
-            BYTE Green = image[i][j].rgbtGreen;
-            BYTE Red = image[i][j].rgbtRed;
-            
+            BYTE Blue = (int)image[i][j].rgbtBlue;
+            BYTE Green = (int)image[i][j].rgbtGreen;
+            BYTE Red = (int)image[i][j].rgbtRed;
+
             BYTE average = (Blue + Green + Red) / 3;
-            
+
             image[i][j].rgbtBlue = average;
             image[i][j].rgbtGreen = average;
             image[i][j].rgbtRed = average;
@@ -27,17 +27,46 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
 // Reflect image horizontally
 void reflect(int height, int width, RGBTRIPLE image[height][width])
 {
+    for (int i = 0; i < height; i++)
+    {
+        int n = width * 2 - 1;
+        for (int j = 0; j < width; j++)
+        {
+            BYTE Blue = image[i][n].rgbtBlue;
+            BYTE Green = image[i][n].rgbtGreen;
+            BYTE Red = image[i][n].rgbtRed;
+
+            image[i][j].rgbtBlue = Blue;
+            image[i][j].rgbtGreen = Green;
+            image[i][j].rgbtRed = Red;
+
+            n--;
+        }
+    }
+
     return;
 }
 
 // Blur image
 void blur(int height, int width, RGBTRIPLE image[height][width])
 {
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            BYTE Blue = image[i][j].rgbtBlue;
+            BYTE Green = image[i][j].rgbtGreen;
+            BYTE Red = image[i][j].rgbtRed;
+            
+            
+        }
+    }
     return;
 }
 
 // Detect edges
 void edges(int height, int width, RGBTRIPLE image[height][width])
 {
+    
     return;
 }
