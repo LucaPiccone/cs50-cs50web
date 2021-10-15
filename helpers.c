d8ec66858f1b2bf2d53d0c5a09a18531aaebcc50
LucaPiccone <LucaPiccone@users.noreply.github.com>
2021-10-15 08:45:45 +0000

    automated commit by check50 [check50=True]

--- helpers.c
+++ helpers.c
@@ -92,62 +92,92 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
                     BYTE blue = image_copy_blue[i][j];
                     BYTE green = image_copy_green[i][j];
                     BYTE red = image_copy_red[i][j];
-                
+
                     BYTE blue_right = image_copy_blue[i][j + 1];
                     BYTE red_right = image_copy_green[i][j + 1];
                     BYTE green_right = image_copy_red[i][j + 1];
-    
+
                     BYTE blue_bottom = image_copy_blue[i + 1][j];
                     BYTE green_bottom = image_copy_green[i + 1][j];
                     BYTE red_bottom = image_copy_red[i + 1][j];
-    
+
                     BYTE blue_bottom_right = image_copy_blue[i + 1][j + 1];
                     BYTE green_bottom_right = image_copy_green[i + 1][j + 1];
                     BYTE red_bottom_right = image_copy_red[i + 1][j + 1];
-    
+
                     BYTE blue_average = round((blue_right + blue_bottom + blue_bottom_right + blue) / 4.0);
                     BYTE green_average = round((green_right + green_bottom + green_bottom_right + green) / 4.0);
                     BYTE red_average = round((red_right + red_bottom + red_bottom_right + red) / 4.0);
-    
+
                     image[i][j].rgbtBlue = blue_average;
                     image[i][j].rgbtGreen = green_average;
                     image[i][j].rgbtRed = red_average;
                 }
-                
+
                 else if (j == last_pixel_in_row)
                 {
                     BYTE blue = image_copy_blue[i][j];
                     BYTE green = image_copy_green[i][j];
                     BYTE red = image_copy_red[i][j];
-                    
+
                     BYTE blue_left = image_copy_blue[i][j - 1];
                     BYTE green_left = image_copy_green[i][j - 1];
                     BYTE red_left = image_copy_red[i][j - 1];
-    
+
                     BYTE blue_bottom = image_copy_blue[i + 1][j];
                     BYTE green_bottom = image_copy_green[i + 1][j];
                     BYTE red_bottom = image_copy_red[i + 1][j];
-    
+
                     BYTE blue_bottom_left = image_copy_blue[i + 1][j - 1];
                     BYTE green_bottom_left = image_copy_green[i + 1][j - 1];
                     BYTE red_bottom_left = image_copy_red[i + 1][j - 1];
-    
+
                     BYTE blue_average = round((blue_left + blue_bottom + blue_bottom_left + blue) / 4.0);
                     BYTE green_average = round((green_left + green_bottom + green_bottom_left + green) / 4.0);
                     BYTE red_average = round((red_left + red_bottom + red_bottom_left + red) / 4.0);
-    
+
                     image[i][j].rgbtBlue = blue_average;
                     image[i][j].rgbtGreen = green_average;
                     image[i][j].rgbtRed = red_average;
                 }
-                
-                else 
+
+                else
                 {
-                    
+                    BYTE blue = image_copy_blue[i][j];
+                    BYTE green = image_copy_green[i][j];
+                    BYTE red = image_copy_red[i][j];
+
+                    BYTE blue_left = image_copy_blue[i][j-1];
+                    BYTE green_left = image_copy_green[i][j-1];
+                    BYTE red_left = image_copy_red[i][j-1];
+
+                    BYTE blue_right = image_copy_blue[i][j+1];
+                    BYTE green_right = image_copy_green[i][j+1];
+                    BYTE red_right = image_copy_red[i][j+1];
+
+                    BYTE blue_bottom_left = image_copy_blue[i+1][j-1];
+                    BYTE green_bottom_left = image_copy_green[i+1][j-1];
+                    BYTE red_bottom_left = image_copy_red[i+1][j-1];
+
+                    BYTE blue_bottom = image_copy_blue[i+1][j];
+                    BYTE green_bottom = image_copy_green[i+1][j];
+                    BYTE red_bottom = image_copy_red[i+1][j];
+
+                    BYTE blue_bottom_right = image_copy_blue[i+1][j+1];
+                    BYTE green_bottom_right = image_copy_green[i+1][j+1];
+                    BYTE red_bottom_right = image_copy_red[i+1][j+1];
+
+                    BYTE blue_average = round((blue + blue_left + blue_right + blue_bottom_left + blue_bottom + blue_bottom_right) / 6.0);
+                    BYTE green_average = round((green + green_left + green_right + green_bottom_left + green_bottom + green_bottom_right) / 6.0);
+                    BYTE red_average = round((red + red_left + red_right + red_bottom_left + red_bottom + red_bottom_right) / 6.0);
+
+                    image[i][j].rgbtBlue = blue_average;
+                    image[i][j].rgbtGreen = green_average;
+                    image[i][j].rgbtRed = red_average;
                 }
 
             }
-            
+
             else if (i == last_row)
             {
                 if (j == first_pixel_in_row)
@@ -155,77 +185,168 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
                     BYTE blue = image_copy_blue[i][j];
                     BYTE green = image_copy_green[i][j];
                     BYTE red = image_copy_red[i][j];
-                
+
                     BYTE blue_right = image_copy_blue[i][j + 1];
                     BYTE red_right = image_copy_green[i][j + 1];
                     BYTE green_right = image_copy_red[i][j + 1];
-    
+
                     BYTE blue_top = image_copy_blue[i - 1][j];
                     BYTE green_top = image_copy_green[i - 1][j];
                     BYTE red_top = image_copy_red[i - 1][j];
-    
+
                     BYTE blue_top_right = image_copy_blue[i - 1][j + 1];
                     BYTE green_top_right = image_copy_green[i - 1][j + 1];
                     BYTE red_top_right = image_copy_red[i - 1][j + 1];
-    
+
                     BYTE blue_average = round((blue_right + blue_top + blue_top_right + blue) / 4.0);
                     BYTE green_average = round((green_right + green_top + green_top_right + green) / 4.0);
                     BYTE red_average = round((red_right + red_top + red_top_right + red) / 4.0);
-    
+
                     image[i][j].rgbtBlue = blue_average;
                     image[i][j].rgbtGreen = green_average;
                     image[i][j].rgbtRed = red_average;
                 }
-                
+
                 else if (j == last_pixel_in_row)
                 {
                     BYTE blue = image_copy_blue[i][j];
                     BYTE green = image_copy_green[i][j];
                     BYTE red = image_copy_red[i][j];
-                
+
                     BYTE blue_left = image_copy_blue[i][j - 1];
                     BYTE green_left = image_copy_green[i][j - 1];
                     BYTE red_left = image_copy_red[i][j - 1];
-    
+
                     BYTE blue_top = image_copy_blue[i - 1][j];
                     BYTE green_top = image_copy_green[i - 1][j];
                     BYTE red_top = image_copy_red[i - 1][j];
-    
+
                     BYTE blue_top_left = image_copy_blue[i - 1][j - 1];
                     BYTE green_top_left = image_copy_green[i - 1][j - 1];
                     BYTE red_top_left = image_copy_red[i - 1][j - 1];
-    
+
                     BYTE blue_average = round((blue_left + blue_top + blue_top_left + blue) / 4.0);
                     BYTE green_average = round((green_left + green_top + green_top_left + green) / 4.0);
                     BYTE red_average = round((red_left + red_top + red_top_left + red) / 4.0);
-    
+
                     image[i][j].rgbtBlue = blue_average;
                     image[i][j].rgbtGreen = green_average;
                     image[i][j].rgbtRed = red_average;
                 }
-                
-                else 
+
+                else
                 {
-                    
+                    BYTE blue = image_copy_blue[i][j];
+                    BYTE green = image_copy_green[i][j];
+                    BYTE red = image_copy_red[i][j];
+
+                    BYTE blue_left = image_copy_blue[i][j-1];
+                    BYTE green_left = image_copy_green[i][j-1];
+                    BYTE red_left = image_copy_red[i][j-1];
+
+                    BYTE blue_right = image_copy_blue[i][j+1];
+                    BYTE green_right = image_copy_green[i][j+1];
+                    BYTE red_right = image_copy_red[i][j+1];
+
+                    BYTE blue_top_left =  image_copy_blue[i-1][j-1];
+                    BYTE green_top_left = image_copy_green[i-1][j-1];
+                    BYTE red_top_left = image_copy_red[i-1][j-1];
+
+                    BYTE blue_top = image_copy_blue[i-1][j];
+                    BYTE green_top = image_copy_green[i-1][j];
+                    BYTE red_top = image_copy_red[i-1][j];
+
+                    BYTE blue_top_right = image_copy_blue[i-1][j+1];
+                    BYTE green_top_right = image_copy_green[i-1][j+1];
+                    BYTE red_top_right = image_copy_red[i-1][j+1];
+
+                    BYTE blue_average = round((blue + blue_left + blue_right + blue_top_left + blue_top + blue_top_right) / 6.0);
+                    BYTE green_average = round((green + green_left + green_right + green_top_left + green_top + green_top_right) / 6.0);
+                    BYTE red_average = round((red + red_left + red_right + red_top_left + red_top + red_top_right) / 6.0);
+
+                    image[i][j].rgbtBlue = blue_average;
+                    image[i][j].rgbtGreen = green_average;
+                    image[i][j].rgbtRed = red_average;
+
                 }
             }
-            
+
             else if (j == first_pixel_in_row)
             {
                 if (i != first_row || i != last_row)
                 {
+                    BYTE blue = image_copy_blue[i][j];
+                    BYTE green = image_copy_green[i][j];
+                    BYTE red = image_copy_red[i][j];
 
+                    BYTE blue_top = image_copy_blue[i-1][j];
+                    BYTE green_top = image_copy_green[i-1][j];
+                    BYTE red_top = image_copy_red[i-1][j];
+
+                    BYTE blue_bottom = image_copy_blue[i+1][j];
+                    BYTE green_bottom = image_copy_green[i+1][j];
+                    BYTE red_bottom = image_copy_red[i+1][j];
+
+                    BYTE blue_top_right = image_copy_blue[i-1][j+1];
+                    BYTE green_top_right = image_copy_green[i-1][j+1];
+                    BYTE red_top_right = image_copy_red[i-1][j+1];
+
+                    BYTE blue_right = image_copy_blue[i][j+1];
+                    BYTE green_right = image_copy_green[i][j+1];
+                    BYTE red_right = image_copy_red[i][j+1];
+
+                    BYTE blue_bottom_right = image_copy_blue[i+1][j+1];
+                    BYTE green_bottom_right = image_copy_green[i+1][j+1];
+                    BYTE red_bottom_right = image_copy_red[i+1][j+1];
+
+                    BYTE blue_average = round((blue + blue_top + blue_bottom + blue_top_right + blue_right + blue_bottom_right) / 6.0);
+                    BYTE green_average = round((green + green_top + green_bottom + green_top_right + green_right + green_bottom_right) / 6.0);
+                    BYTE red_average = round((red + red_top + red_bottom + red_top_right + red_right + red_bottom_right) / 6.0);
+
+                    image[i][j].rgbtBlue = blue_average;
+                    image[i][j].rgbtGreen = green_average;
+                    image[i][j].rgbtRed = red_average;
                 }
             }
-            
+
             else if (j == last_pixel_in_row)
             {
                 if (i != first_row || i != last_row)
                 {
+                    BYTE blue = image_copy_blue[i][j];
+                    BYTE green = image_copy_green[i][j];
+                    BYTE red = image_copy_red[i][j];
 
+                    BYTE blue_top = image_copy_blue[i-1][j];
+                    BYTE green_top = image_copy_green[i-1][j];
+                    BYTE red_top = image_copy_red[i-1][j];
+
+                    BYTE blue_bottom = image_copy_blue[i+1][j];
+                    BYTE green_bottom = image_copy_green[i+1][j];
+                    BYTE red_bottom = image_copy_red[i+1][j];
+
+                    BYTE blue_top_left =  image_copy_blue[i-1][j-1];
+                    BYTE green_top_left = image_copy_green[i-1][j-1];
+                    BYTE red_top_left = image_copy_red[i-1][j-1];
+
+                    BYTE blue_left = image_copy_blue[i][j-1];
+                    BYTE green_left = image_copy_green[i][j-1];
+                    BYTE red_left = image_copy_red[i][j-1];
+
+                    BYTE blue_bottom_left = image_copy_blue[i+1][j-1];
+                    BYTE green_bottom_left = image_copy_green[i+1][j-1];
+                    BYTE red_bottom_left = image_copy_red[i+1][j-1];
+
+                    BYTE blue_average = round((blue + blue_top + blue_bottom + blue_top_left + blue_left + blue_bottom_left) / 6.0);
+                    BYTE green_average = round((green + green_top + green_bottom + green_top_left + green_left + green_bottom_left) / 6.0);
+                    BYTE red_average = round((red + red_top + red_bottom + red_top_left + red_left + red_bottom_left) / 6.0);
+
+                    image[i][j].rgbtBlue = blue_average;
+                    image[i][j].rgbtGreen = green_average;
+                    image[i][j].rgbtRed = red_average;
                 }
             }
-            
+
             else
             {
                 BYTE blue_top_left =  image_copy_blue[i-1][j-1];
