0f416c7ed87ad706566936632999f7ae8f9d5753
LucaPiccone <LucaPiccone@users.noreply.github.com>
2021-10-28 15:42:09 +0000

    automated commit by check50 [check50=True]

--- recover.c
+++ recover.c
@@ -24,56 +24,51 @@ int main(int argc, char *argv[])
         return 1;
     }
 
+    BYTE buffer[JPEG_SIZE];
 
     char jpeg[50][12];
     FILE *outfile[50];
-    for (int i = 0; i < 50; i++)
-    {
-        if (i < 10)
-        {
-            sprintf(jpeg[i], "00%i.jpeg", i);
-        }
-        else
-        {
-            sprintf(jpeg[i], "0%i.jpeg", i);
-        }
-
-        outfile[i] = fopen(jpeg[i], "w");
-        if (!outfile[i])
-        {
-            return 1;
-        }
-    }
-
-    BYTE buffer[JPEG_SIZE];
-    BYTE header[HEADER_SIZE];
 
     int jpeg_count = 0;
-    int count = 0;
+    int i = 0;
     while(fread(buffer, JPEG_SIZE, 1, infile))
     {
         if (buffer[0] == 255 && buffer[1] == 216 && buffer[2] == 255 && (buffer[3] & 0xfe) == 224)
         {
-            jpeg_count++;
-            fwrite(buffer, JPEG_SIZE, 1, outfile[jpeg_count-1]);
+            i++;
+            if (i <= 10)
+            {
+                sprintf(jpeg[i-1], "00%i.jpeg", i-1);
+            }
+            else
+            {
+                sprintf(jpeg[i-1], "0%i.jpeg", i-1);
+            }
+
+            if (i == 1)
+            {
+                // first jpeg
+                outfile[i-1] = fopen(jpeg[i-1], "w");
+
+                fwrite(buffer, JPEG_SIZE, 1, outfile[i-1]);
+            }
+            else
+            {
+                // not first jpeg
+                //close last jpeg
+                fclose(outfile[i-2]);
+                outfile[i-1] = fopen(jpeg[i-1], "w");
+
+                fwrite(buffer, JPEG_SIZE, 1, outfile[i-1]);
+            }
         }
         else
         {
             // Already found jpeg
-            if (jpeg_count > 0)
+            if (i > 0)
             {
-                fwrite(buffer, JPEG_SIZE, 1, outfile[jpeg_count-1]);
+                fwrite(buffer, JPEG_SIZE, 1, outfile[i-1]);
             }
         }
     }
-
-    printf("%i\n", jpeg_count);
-
-    for (int i = 0; i < 50; i++)
-    {
-        fclose(outfile[i]);
-    }
-
-    fclose(infile);
-
 }
