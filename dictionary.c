a408b0ca3646deba1292340239109c32c5163bba
LucaPiccone <LucaPiccone@users.noreply.github.com>
2021-11-21 06:58:16 +0000

    automated commit by check50 [check50=True]

--- dictionary.c
+++ dictionary.c
@@ -5,6 +5,7 @@
 #include <stdbool.h>
 #include <string.h>
 #include <strings.h>
+#include <stdint.h>
 
 #include "dictionary.h"
 
@@ -17,7 +18,7 @@ typedef struct node
 node;
 
 // Number of buckets in hash table
-const unsigned int N = 26;
+const unsigned int N = 216000;
 
 // Hash table
 node *table[N];
@@ -25,26 +26,30 @@ node *table[N];
 // Dictionary word count
 int word_count = 0;
 
+typedef uint64_t EIGHT_BYTES;
+
 // Returns true if word is in dictionary, else false
 bool check(const char *word)
 {
     // TODO
     int index = hash(word);
 
-    //printf("%s\n", word);
-    //printf("%i\n", index);
-    
+    // printf("%s\n", word);
+    // printf("%i\n", index);
+
+    if (table[index] == NULL)
+        return false;
+
     node *tmp = table[index];
-    
+
     while (strcasecmp(tmp->word, word) != 0)
     {
-        tmp = tmp->next;
-        if(tmp == NULL)
-        {
+        if (tmp->next != NULL)
+            tmp = tmp->next;
+        else
             return false;
-        }
     }
-    
+
     return true;
 }
 
@@ -52,20 +57,37 @@ bool check(const char *word)
 unsigned int hash(const char *word)
 {
     // TODO
-    char copy;
-    int hash;
-    if (islower(word[0]))
+    // HASH FUNCTION:
+    // https://en.wikipedia.org/wiki/Fowler%E2%80%93Noll%E2%80%93Vo_hash_function#FNV-1a_hash
+
+    int length = strlen(word) + 1;
+
+    char copy[length];
+    for (int i = 0; i < length; i++)
     {
-        copy = word[0];
-        hash = copy - 'a';
-    }
-    else
-    {
-        copy = word[0] + 32;
-        hash = copy - 'a';
+        if (islower(word[i]))
+        {
+            copy[i] = word[i];
+        }
+        else
+        {
+            copy[i] = (word[i] + 32);
+        }
     }
 
-    return hash;
+    EIGHT_BYTES fnv_prime = 14695981039346656037U;
+    EIGHT_BYTES hash = 14695981039346656037U;
+
+    for (int i = 0; i < length; i++)
+    {
+        copy[i] *= 7;
+        hash = hash * fnv_prime;
+        hash = hash ^ copy[i];
+    }
+
+    hash = hash % 216000;
+
+    return (hash);
 }
 
 // Loads dictionary into memory, returning true if successful, else false
@@ -79,11 +101,8 @@ bool load(const char *dictionary)
     }
 
     char word[LENGTH + 1];
-
     while (fscanf(dict, "%s", word) == 1)
     {
-        int length = strlen(word) + 1;
-
         node *n = malloc(sizeof(node));
         if (n == NULL)
         {
@@ -91,54 +110,14 @@ bool load(const char *dictionary)
         }
 
         n->next = NULL;
-
         strcpy(n->word, word);
 
-        //printf("%s\n", word);
-        //printf("%s\n", n->word);
-
         int index = hash(n->word);
-
-        // printf("%i\n", index);
-
         n->next = table[index];
-
         table[index] = n;
 
         word_count++;
     }
-
-    printf("Start");
-
-    int count[N];
-    for (int i = 0; i < N; i++)
-    {
-        count[i] = 0;
-    }
-
-    for (int i = 0; i < N; i++)
-    {
-        if (table[i] == NULL)
-        {
-            printf("\t%i\t =>\n", i);
-        }
-        else
-        {
-            printf("\t%i\t", i);
-
-            node *tmp = table[i];
-
-            while (tmp != NULL)
-            {
-                count[i]++;
-                tmp = tmp->next;
-            }
-            printf("count %i\n", count[i]);
-        }
-    }
-
-    printf("End\n");
-
 
     fclose(dict);
     return true;
