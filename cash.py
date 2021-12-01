cc35f21170fc1f165b2133e0be65c54665d51cf7
LucaPiccone <LucaPiccone@users.noreply.github.com>
2021-12-01 17:54:50 +0000

    automated commit by check50 [check50=True]

---
+++ cash.py
@@ -0,0 +1,65 @@
+from cs50 import get_float 
+from math import floor 
+
+def main():
+    while True:
+        owed = get_float("Change owed: ")
+        if owed > 0:
+            break
+    
+    change = 0.00
+    coins = 0
+    
+    while True:
+        change += 0.25 
+        coins += 1
+        change = round(change, 3)
+        if change > owed:
+            change -= 0.25
+            coins -= 1
+            break
+        elif change == owed:
+            break
+    
+    while True:
+        change += 0.10
+        coins += 1
+        change = round(change, 3)
+        if change > owed:
+            change -= 0.10
+            coins -= 1
+            break
+        elif change == owed:
+            break
+        
+    while True:
+        change += 0.05
+        coins += 1
+        change = round(change, 3)
+        if change > owed:
+            change -= 0.05
+            coins -= 1
+            break
+        elif change == owed:
+            break
+        
+    while True:
+        change += 0.01
+        coins += 1
+        change = round(change, 3)
+        if change > owed:
+            change -= 0.01
+            coins -= 1
+            break
+        elif change == owed:
+            break
+        
+    
+        
+    
+    print(owed)
+    print(change)
+    print(coins)
+
+
+main()
