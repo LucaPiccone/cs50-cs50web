d5e27cfca69d4ced16cc73d565923bfe5ea217ce
LucaPiccone <LucaPiccone@users.noreply.github.com>
2021-12-01 17:18:13 +0000

    automated commit by submit50

--- mario.py
+++ mario.py
@@ -9,15 +9,16 @@ width = height * 2 + 2
 
 for y in range(height):
     for x in range(width):
+        # Middle: print spaces 
         if x == height or x == height + 1:
             print(" ", end="")
+        # Right side: height + 2 (brings us to right side) - 
+        # If x is greater than y + height + 2 print nothing 
         elif y + height + 2 < x:
             print("", end="")
+        # Left side: print spaces 
         elif y + x < height - 1:
             print(" ", end="")
         else:
             print("#", end="")
     print()
-        
-
-
