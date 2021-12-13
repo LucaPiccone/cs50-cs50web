feee1457280f38da47b6d0e76bceff41af86da9e
LucaPiccone <LucaPiccone@users.noreply.github.com>
2021-12-13 03:27:50 +0000

    automated commit by check50 [check50=True]

--- 13.sql
+++ 13.sql
@@ -1,1 +1,1 @@
-sqlite> SELECT name FROM people JOIN stars ON people.id = stars.person_id JOIN movies ON stars.movie_id = movies.id WHERE stars.person_id = (movies.id = (people.name = "Kevin Bacon"));
+SELECT name FROM people WHERE id IN (SELECT person_id FROM stars WHERE movie_id IN (SELECT id FROM movies WHERE id IN (SELECT movie_id FROM stars WHERE person_id IN (SELECT id FROM people WHERE name = "Kevin Bacon"))));
